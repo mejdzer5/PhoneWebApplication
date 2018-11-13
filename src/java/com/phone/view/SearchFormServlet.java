7fdc900a0f3684202e1da51c84d137699abeb534
User <User@Lenovo-PC>
2018-11-13 20:38:32 +0100

    new commit

--- src/java/com/phone/view/SearchFormServlet.java
+++ src/java/com/phone/view/SearchFormServlet.java
@@ -35,7 +35,7 @@ public class SearchFormServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet SearchFormServlet</title>");            
+            out.println("<title>Servlet SearchFormServlet!!!</title>");            
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet SearchFormServlet at " + request.getContextPath() + "</h1>");
